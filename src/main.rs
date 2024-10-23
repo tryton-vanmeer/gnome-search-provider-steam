fd8b7d5079d298b80eda700e90861ae5210a5c7f
Tryton Van Meer <code@trytonvanmeer.dev>
2024-10-23 14:42:29 -0400

    feat: use tracing crate for logging
    Cloes #2

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    io::{self, IsTerminal},
+};
 
 use anyhow::Result;
 use search_provider::{ResultID, ResultMeta, SearchProvider, SearchProviderImpl};
 use steamlocate::SteamDir;
+use tracing::{debug, error};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 type GameResults = HashMap<String, String>;
 
@@ -25,6 +30,7 @@ impl SearchProviderImpl for Application {
             let name_lower = name.to_lowercase();
             for term in terms {
                 if name_lower.contains(&term.to_lowercase()) {
+                    debug!("found game '{}' ({}) for term '{}'", name, id, term);
                     results.push(id.clone());
                 }
             }
@@ -55,11 +61,11 @@ fn get_games() -> Result<GameResults> {
 
     for library in steam.libraries()? {
         match library {
-            Err(err) => eprintln!("failed reading library: {err}"),
+            Err(err) => error!("failed reading library: {err}"),
             Ok(library) => {
                 for app in library.apps() {
                     match app {
-                        Err(err) => eprintln!("failed reading app: {err}"),
+                        Err(err) => error!("failed reading app: {err}"),
                         Ok(app) => {
                             results.insert(app.app_id.to_string(), app.name.unwrap());
                         }
@@ -74,6 +80,14 @@ fn get_games() -> Result<GameResults> {
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    if io::stdin().is_terminal() {
+        tracing_subscriber::fmt::init();
+    } else {
+        tracing_subscriber::registry()
+            .with(tracing_journald::layer()?)
+            .init();
+    }
+
     let app = Application {
         games: get_games()?,
     };
